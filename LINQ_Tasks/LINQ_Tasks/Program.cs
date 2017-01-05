db495a1368895dc584c44c90fd8aa55aef8c1eca
Grant Smith <smithge87@gmail.com>
2017-01-05 16:18:42 -0600

    first task completed

---
+++ LINQ_Tasks/LINQ_Tasks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>() { "cat", "cat","dog", "dog", "dog", "dog", "bird" };
+            List<string> sortedWords;
+
+            sortedWords = words.Distinct().ToList();
+            sortedWords.ForEach((x) => { Console.WriteLine(x);});
+            Console.ReadLine();
+        }
+    }
+}
+
