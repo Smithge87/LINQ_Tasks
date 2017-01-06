96c7cde64c41293fdcb32de5e9889d7b973f5e85
Grant Smith <smithge87@gmail.com>
2017-01-06 07:24:09 -0600

    half way through task two

--- LINQ_Tasks/LINQ_Tasks/Program.cs
+++ LINQ_Tasks/LINQ_Tasks/Program.cs
@@ -16,7 +16,47 @@ namespace LINQ_Tasks
             sortedWords = words.Distinct().ToList();
             sortedWords.ForEach((x) => { Console.WriteLine(x);});
             Console.ReadLine();
+
+            List<string> grades = new List<string>() { "20", "10", "30", "40", "50", "60", "70", "80", "90", "100" };
+            List<int> gradesCleaned;
+
+            gradesCleaned = grades.ConvertAll(x => Int32.Parse(x));
+            gradesCleaned.Sort();
+            gradesCleaned.RemoveAt(0);
+            double average = gradesCleaned.Average();
+            //gradesCleaned.ForEach((x) => { Console.WriteLine(x); });
+            Console.WriteLine("\n"+average);
+            Console.ReadLine();
+
+            List<string> moreGrades = new List<string>();
+            List<string> cleanedGrades = new List<string>();
+            moreGrades.Add("70,20,30,40,50,60,70");
+            moreGrades.Add("80,20,30,40,50,60,70");
+            moreGrades.Add("100,20,30,40,50,60,70");
+            moreGrades.Add("90,20,30,40,50,60,70");
+            List<string> sortedGrades = new List<string>();
+
+
+            foreach (string s in moreGrades)
+            {
+                cleanedGrades = s.Split(',').Select(y => y.Trim()).ToList();
+                sortedGrades.AddRange(cleanedGrades);
+                sortedGrades.Sort();
+                sortedGrades.ConvertAll(x => Int32.Parse(x));
+                sortedGrades.RemoveAt(0);
+            }
+            
+            Console.ReadLine();
+
+
+
         }
     }
 }
+//target = orig.ConvertAll(x => new TargetType { SomeValue = x.SomeValue });
+//1. Write a function that takes in a list of strings and returns a copy of the list without duplicates.
 
+//2. Write a function that calculates the class grade average after dropping the lowest grade for each student.That is:
+//Write a function that takes in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), drops the lowest grade from each string, averages the rest of the grades from that string, then averages the averages.
+
+//3. Write a function that takes in a string of letters (e.g., "Llewellyn") and returns a alphabetically ordered string corresponding to the letter frequency(e.g., "E2L4N1W1Y1")
