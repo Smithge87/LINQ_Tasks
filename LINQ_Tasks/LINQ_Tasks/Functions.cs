47e4b8ec0e1104408b47c40b4216261cad5b35b8
Grant Smith <smithge87@gmail.com>
2017-01-06 09:19:18 -0600

    second function finished, all parts moved off of program class

---
+++ LINQ_Tasks/LINQ_Tasks/Functions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Tasks
+{
+    class Functions
+    {
+        public List<string> RemoveDuplictes(List<string>toClean)
+        {
+            List<string>sortedWords = toClean.Distinct().ToList();
+            return sortedWords;
+        }
+        public double DropAndAverage(List<string> grades)
+        {
+            List<double> averages = new List<double>();
+            foreach (string s in grades)
+            {
+                List<string>cleanedGrades = s.Split(',').Select(y => y.Trim()).ToList();
+                List<int>sortedGrades = cleanedGrades.ConvertAll(x => Int32.Parse(x));
+                sortedGrades.Sort();
+                sortedGrades.RemoveAt(0);
+                averages.Add(sortedGrades.Average());
+            }
+            double average = Math.Round(averages.Average(), 2, MidpointRounding.AwayFromZero);
+            return average;
+        }
+    }
+}
--- LINQ_Tasks/LINQ_Tasks/Program.cs
+++ LINQ_Tasks/LINQ_Tasks/Program.cs
@@ -6,47 +6,30 @@ using System.Threading.Tasks;
 
 namespace LINQ_Tasks
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
+            Functions functions = new Functions();
+
+            //1. Write a function that takes in a list of strings and returns a copy of the list without duplicates.
+
             List<string> words = new List<string>() { "cat", "cat","dog", "dog", "dog", "dog", "bird" };
-            List<string> sortedWords;
-
-            sortedWords = words.Distinct().ToList();
-            sortedWords.ForEach((x) => { Console.WriteLine(x);});
+            words = functions.RemoveDuplictes(words);
+            words.ForEach((x) => { Console.WriteLine(x);});
             Console.ReadLine();
 
-            List<string> grades = new List<string>() { "20", "10", "30", "40", "50", "60", "70", "80", "90", "100" };
-            List<int> gradesCleaned;
+            //2. Write a function that calculates the class grade average after dropping the lowest grade for each student.That is:
+            //Write a function that takes in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), drops the lowest grade from each string, averages the rest of the grades from that string, then averages the averages.
 
-            gradesCleaned = grades.ConvertAll(x => Int32.Parse(x));
-            gradesCleaned.Sort();
-            gradesCleaned.RemoveAt(0);
-            double average = gradesCleaned.Average();
-            //gradesCleaned.ForEach((x) => { Console.WriteLine(x); });
-            Console.WriteLine("\n"+average);
-            Console.ReadLine();
-
-            List<string> moreGrades = new List<string>();
-            List<string> cleanedGrades = new List<string>();
-            moreGrades.Add("70,20,30,40,50,60,70");
-            moreGrades.Add("80,20,30,40,50,60,70");
-            moreGrades.Add("100,20,30,40,50,60,70");
-            moreGrades.Add("90,20,30,40,50,60,70");
-            List<string> sortedGrades = new List<string>();
-
-
-            foreach (string s in moreGrades)
-            {
-                cleanedGrades = s.Split(',').Select(y => y.Trim()).ToList();
-                sortedGrades.AddRange(cleanedGrades);
-                sortedGrades.Sort();
-                sortedGrades.ConvertAll(x => Int32.Parse(x));
-                sortedGrades.RemoveAt(0);
-            }
-            
-            Console.ReadLine();
+            List<string> grades = new List<string>();
+            grades.Add("70,20,30,40,50,60,70");
+            grades.Add("80,20,30,40,50,60,70");
+            grades.Add("100,20,30,40,50,60,70");
+            grades.Add("90,20,30,40,50,60,70");
+            double averages = functions.DropAndAverage(grades);
+            Console.WriteLine(averages);
+            Console.ReadLine(); 
 
 
 
@@ -54,7 +37,6 @@ namespace LINQ_Tasks
     }
 }
 //target = orig.ConvertAll(x => new TargetType { SomeValue = x.SomeValue });
-//1. Write a function that takes in a list of strings and returns a copy of the list without duplicates.
 
 //2. Write a function that calculates the class grade average after dropping the lowest grade for each student.That is:
 //Write a function that takes in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), drops the lowest grade from each string, averages the rest of the grades from that string, then averages the averages.
